49788d3e6be1d24a4f20cc96ac55054398042022
kartheeswaranAR <kartheeswaran.dev@outloook.com>
2025-12-11 10:59:54 +0530

    RK_E018-Resolved the issue

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import express, { NextFunction, Request, Response } from "express";
 // import { VercelRequest, VercelResponse } from "@vercel/node";
-// import { SpeedInsights } from "@vercel/speed-insights/next";
-import { injectSpeedInsights } from "@vercel/speed-insights/node";
 import mysql, { Connection, RowDataPacket } from "mysql2/promise";
 import axios from "axios";
 import cors from "cors";
@@ -65,9 +63,6 @@ const bcrypt = require("bcryptjs");
 const specs = swaggerJsdoc(options);
 const app = express();
 // const port = process.env.PORT || 5253; // Include the port variable in .env, if you need to run on the different port
-
-// Initialize Vercel Speed Insights
-injectSpeedInsights();
 
 app.use(
   cors({
